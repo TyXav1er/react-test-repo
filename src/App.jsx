aa5db95c754e8aac5469e58f3562a1500613992f
TyXav1er <Tyler.x.j2020@gmail.com>
2024-10-14 15:11:58 -0400

    Learned useContext and added MyComponent files a-d

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 // useContext() = React Hook that allows you to share values between multiple levels of components without passing props through each level
 
-import DigitalClock from "./Components/DigitalClock";
+// PROVIDER COMPONENT
+// 1. import {createContext} from 'react'
+// 2. export const MyContext = createContext()
+// 3. <myCOntext.Provider value={value}
+// <Child />
+// </MyContext.Provider>
+
+// CONSUMER COMPONENTS
+// 1. import React, { useContext } from 'react'
+//    import { MyContext } from './ComponentA'
+// 2. const value = useContext(MyContext)
+
+import ComponentA from "./Components/ComponentA";
+import React from "react";
 
 function App() {
-  return (<DigitalClock />)
+  return (<>
+    <ComponentA />
+  </>)
 }
 
 export default App;
